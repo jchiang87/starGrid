478a61ab0890e462d7b4d244512f6d70cf27064f
James Chiang <jchiang87@gmail.com>
2024-09-29 14:21:32 -0700

    remove unneeded import and explicitly ask for star_grid objects

--- examples/read_star_grid_catalog.py
+++ examples/read_star_grid_catalog.py
@@ -1,6 +1,5 @@
 from skycatalogs import skyCatalogs
 from skycatalogs.utils import Disk
-from star_grid import StarGridObject, StarGridCollection
 
 yaml_file = "./skyCatalog.yaml"
 skycatalog_root = "."
@@ -10,7 +9,7 @@ sky_cat = skyCatalogs.open_catalog(yaml_file, skycatalog_root=skycatalog_root)
 ra0, dec0 = 9.5, -44.0
 radius = 0.17*3600.0
 region = Disk(ra0, dec0, radius)
-objs = sky_cat.get_objects_by_region(region)
+objs = sky_cat.get_objects_by_region(region, obj_type_set={'star_grid'})
 
 print(len(objs))
 print(objs[0:20])
